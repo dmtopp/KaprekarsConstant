48a8c3877a2063f4d0a1e0a1095407390d46ab4d
Daniel Topp <dmtopp@gmail.com>
2018-08-09 21:36:33 -0500

    Initial commit.

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,7 +1,57 @@
-import placeholder from '../index'
+import KaprekarsConstant, { arrangeAsc, arrangeDsc, padZeros, subtractSmallerFromLarger } from '../index'
 
-describe('(placeholder test)', () => {
+describe.skip('(placeholder test)', () => {
   it('should not throw', () => {
-    expect(placeholder).not.toThrow()
+    expect(KaprekarsConstant).not.toThrow()
+  })
+})
+
+describe('(Helper) arrangeAsc', () => {
+  it('should sort the digits of a number', () => {
+    expect(arrangeAsc('3521')).toBe('1235')
+  })
+
+  it('should sort zeros to the beginning', () => {
+    expect(arrangeAsc('3501')).toBe('0135')
+  })
+})
+
+describe('(Helper) arrangeDsc', () => {
+  it('should sort the digits of a number', () => {
+    expect(arrangeDsc('3521')).toBe('5321')
+  })
+
+  it('should sort zeros to the beginning', () => {
+    expect(arrangeDsc('3501')).toBe('5310')
+  })
+})
+
+describe('(Helper) padZeros', () => {
+  it('should add zeros until the number is four digits', () => {
+    expect(padZeros('1')).toBe('0001')
+    expect(padZeros('11')).toBe('0011')
+    expect(padZeros('111')).toBe('0111')
+    expect(padZeros('1111')).toBe('1111')
+  })
+})
+
+describe('(Helper) subtractSmallerFromLarger', () => {
+  it('should return the diff of the larger number minus the smaller number', () => {
+    expect(subtractSmallerFromLarger('1000', '0050')).toBe('0950')
+    expect(subtractSmallerFromLarger('0050', '1000')).toBe('0950')
+  })
+})
+
+describe('(Test cases)', () => {
+  it('should pass test case 1', () => {
+    expect(KaprekarsConstant(3524)).toBe(3)
+  })
+
+  it('should pass test case 2', () => {
+    expect(KaprekarsConstant(2111)).toBe(5)
+  })
+
+  it('should pass test case 3', () => {
+    expect(KaprekarsConstant(9831)).toBe(7)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-export default () => {}
+const MAGIC_NUMBER = '6174'
+const int = str => parseInt(str, 10)
+
+// 1) arrange in ascending order
+export const arrangeAsc = numberStr =>
+  `${numberStr}`.split('').sort((a, b) => a - b).join('')
+
+// 2) arrange in descending order
+export const arrangeDsc = numberStr =>
+  `${numberStr}`.split('').sort((a, b) => b - a).join('')
+
+// 3) add 0s to make two four-digit numbers
+export const padZeros = numberStr =>
+  (`${numberStr}`.length < 4 ? padZeros(`0${numberStr}`) : numberStr)
+
+// 4) subtract the smaller number from the bigger number (abs(one - two))?
+export const subtractSmallerFromLarger = (num1, num2) => padZeros(`${Math.abs(int(num1) - int(num2))}`)
+
+// 5) repeat subtraction until difference reaches 6174
+// 6) return number of iterations until 6174
+export default (number) => {
+  let numberOfSteps = 0
+  let result = number
+
+  while (result !== MAGIC_NUMBER) {
+    const asc = padZeros(arrangeAsc(result))
+    const dsc = padZeros(arrangeDsc(result))
+
+    result = subtractSmallerFromLarger(asc, dsc)
+    numberOfSteps += 1
+
+    if (numberOfSteps > 10) {
+      break
+    }
+  }
+
+  return numberOfSteps
+}
